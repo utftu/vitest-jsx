7c64d486ce83d163e45d4c3ed6e36c6409740f62
utftufutukgyftryidytftuv <overby999@gmail.com>
2023-09-18 23:28:12 +0300

    18.09.23 23:28

--- vite.config.js
+++ vite.config.js
@@ -2,28 +2,8 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 
-const a = ['./src/jsx/jsx-dev-runtime']
-
 export default defineConfig({
   plugins: [react({jsxImportSource: './src/jsx'})],
-  test: {
-    server: {
-      deps: {
-        external: a
-      }
-    },
-
-    deps: {
-      optimizer: {
-        ssr: {
-          include: a
-        },
-        web: {
-          include: a
-        },
-      }
-    }
-  }
   // test: {
   //   deps: {
       
